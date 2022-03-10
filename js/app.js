8c33c2a717bd5a8b4660688eb36e762d6d7b54ca
John Doe <hamedhasan422421@gmail.com>
2022-03-10 22:37:53 +0300

    five issue fixed reported

--- js/app.js
+++ js/app.js
@@ -21,13 +21,18 @@ const addToLiked = (id) => {
 };
 
 const reportPost = (id) => {
-    reportedPostsId.push(id);
-    const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
+  const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
+  reportedPostsId.push(id);
     showPosts(remainingPosts);
 };
 
 const displayContent = (text) => {
     return text < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+//  if(text.length < 30){
+//    return text = 'hell'
+//  }else{
+//   return text.slice(0, 30)  + "<span class='fw-bold'>... read more</span>"
+//  }
 };
 
 const switchTab = (id) => {
@@ -151,7 +156,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
