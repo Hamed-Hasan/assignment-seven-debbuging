6a098113c3188ab9e6b3c3b34002f4962bb698af
John Doe <hamedhasan422421@gmail.com>
2022-03-11 10:12:36 +0300

    completed Debug

--- js/app.js
+++ js/app.js
@@ -27,13 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    // return text < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-    return text.length < 30 ?  text  : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>"  ;
-//  if(text.length < 30){
-//    return text = 'hell'
-//  }else{
-//   return text.slice(0, 30)  + "<span class='fw-bold'>... read more</span>"
-//  }
+    return text.length < 30 ?  text  : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
