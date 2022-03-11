60c5465f1e92babcedb92345a1f082adbb4b1f10
John Doe <hamedhasan422421@gmail.com>
2022-03-11 10:47:37 +0300

    Debug Completed

--- js/app.js
+++ js/app.js
@@ -1,65 +1,67 @@
-let posts=[];
+    // ISSUE 1 Line: 26
+    // Issue 2 User Image Line: 72
+    // Issue 3 Line: 37
+    // Issue 4 Who Comment - What Comment Line: 129 - 131
+    // Issue 5 Line: 32 - 163
+    // Issue 6 Line: 152 - 161
 
-const likedPostsId = [];
-const reportedPostsId = [];
+    let posts = [];
 
-const getLikedPosts = () => {
-    return posts.filter((post) => likedPostsId.includes(post.id));
-};
+    const likedPostsId = [];
+    const reportedPostsId = [];
 
-const getReportedPosts = () => {
-    return posts.filter((post) => reportedPostsId.includes(post.id));
-};
+    const getLikedPosts = () => {
+      return posts.filter((post) => likedPostsId.includes(post.id));
+    };
 
-const isLiked = (id) => {
-    return likedPostsId?.length && !!likedPostsId.includes(id);
-};
+    const getReportedPosts = () => {
+      return posts.filter((post) => reportedPostsId.includes(post.id));
+    };
 
-const addToLiked = (id) => {
-    // ISSUE 1 Line: 20
-    likedPostsId.push(id); 
-    showPosts(posts);
-};
+    const isLiked = (id) => {
+      return likedPostsId ?.length && !!likedPostsId.includes(id);
+    };
 
-const reportPost = (id) => {
-  // Issue 5 Line: 27
-  const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
-  reportedPostsId.push(id);
-    showPosts(remainingPosts);
-};
+    const addToLiked = (id) => {
+      likedPostsId.push(id);
+      showPosts(posts);
+    };
 
-const displayContent = (text) => {
-  // Issue 3 Line: 33
-    return text.length < 30 ?  text  : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-};
+    const reportPost = (id) => {
+      const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
+      reportedPostsId.push(id);
+      showPosts(remainingPosts);
+    };
 
-const switchTab = (id) => {
-    if (id === "posts") {
-        document.getElementById( "posts" ).style.display = "grid";
-        document.getElementById( "liked" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
-    } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
+    const displayContent = (text) => {
+      return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    };
+
+    const switchTab = (id) => {
+      if (id === "posts") {
+        document.getElementById("posts").style.display = "grid";
+        document.getElementById("liked").style.display = "none";
+        document.getElementById("reported").style.display = "none";
+      } else if (id === "liked") {
+        document.getElementById("liked").style.display = "block";
+        document.getElementById("posts").style.display = "none";
+        document.getElementById("reported").style.display = "none";
 
         displayLikedPosts();
-    } else {
-        document.getElementById( "reported" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "liked" ).style.display = "none";
+      } else {
+        document.getElementById("reported").style.display = "block";
+        document.getElementById("posts").style.display = "none";
+        document.getElementById("liked").style.display = "none";
 
         displayReportedPosts();
-    }
-};
+      }
+    };
 
-const createPost = (post) => {
-    const image = post.image;
-    const div = document.createElement( "article" );
-    div.classList.add( "post" );
-    // Issue 2 User Image Line: 70
-    // Issue 4 Who Comment - What Comment Line: 127 - 129
-    div.innerHTML = `
+    const createPost = (post) => {
+      const image = post.image;
+      const div = document.createElement("article");
+      div.classList.add("post");
+      div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
                   <a
@@ -133,44 +135,41 @@ const createPost = (post) => {
                 </div>
               </div>
       `;
-    return div;
-};
+      return div;
+    };
 
-const showPosts = (posts) => {
-    const productsContainer = document.getElementById( "posts" );
-    productsContainer.innerHTML = "";
+    const showPosts = (posts) => {
+      const productsContainer = document.getElementById("posts");
+      productsContainer.innerHTML = "";
 
-    posts.forEach((post) => {
+      posts.forEach((post) => {
         const div = createPost(post);
         productsContainer.appendChild(div);
-    });
-};
+      });
+    };
 
-const displayLikedPosts = () => {
-  // Issue 6 Line: 151
-  document.getElementById( "liked" ).innerHTML = ''
-    const likedPosts = getLikedPosts();
-    likedPosts.forEach((post) => {
+    const displayLikedPosts = () => {
+      document.getElementById("liked").innerHTML = ''
+      const likedPosts = getLikedPosts();
+      likedPosts.forEach((post) => {
         const div = createPost(post);
-        document.getElementById( "liked" ).appendChild(div);
-    });
-};
+        document.getElementById("liked").appendChild(div);
+      });
+    };
 
-const displayReportedPosts = () => {
-    // Issue 6 Line: 161
-  document.getElementById( "reported" ).innerHTML = ''
-    const reportedPosts = getReportedPosts();
-    // Issue 5 Line: 164
-    reportedPosts.forEach((post) => {
+    const displayReportedPosts = () => {
+      document.getElementById("reported").innerHTML = ''
+      const reportedPosts = getReportedPosts();
+      reportedPosts.forEach((post) => {
         const div = createPost(post);
-        document.getElementById( "reported" ).appendChild(div);
-    });
-};
+        document.getElementById("reported").appendChild(div);
+      });
+    };
 
-const loadPosts = async () =>{
-  let data = await fetch('../data/posts.json');
-    posts = await data.json();
-  showPosts(posts);
-}
+    const loadPosts = async () => {
+      let data = await fetch('../data/posts.json');
+      posts = await data.json();
+      showPosts(posts);
+    }
 
-loadPosts();
+    loadPosts();
